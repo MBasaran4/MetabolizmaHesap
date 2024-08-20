417bee9de0411e0ebd9e951e438021164a3e1bb4
Mücahit <basaranmucahit785@gmail.com>
2024-08-20 13:39:34 +0300

    Mobile Navbar

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './MainPage.css'; 
-import Navbar from '../../../hesapkitap/src/components/Navbar';
+import Navbar from './Navbar';
 
 
 function MainPage() {
